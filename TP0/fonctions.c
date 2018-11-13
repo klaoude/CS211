9241d025c2289fe43ac3c1c68c5dfb1139ea508a
userir <userir@strech.esisar.fr>
2018-11-13 16:55:26 +0100

    TP0

--- TP0/fonctions.c
+++ TP0/fonctions.c
@@ -21,7 +21,7 @@ int compare(int* a, int *b, int n)
 		}
 	}
 	printf("Comparison OK\n\n");
-	return -1;	
+	return -1;
 }
 
 void swap(int* a, int* b)
@@ -40,12 +40,106 @@ int BubbleSort(int* tab, int n)
 		swapNum = 0;
 		for(int i = 0; i < n - 1; i++)
 		{
+			ret++;
 			if(tab[i] > tab[i+1])
 			{
 				swap(tab + i, tab + i + 1);
 				swapNum++;
 			}
-			ret++;
 		}
 	}
+
+	return ret;
+}
+
+int insertionSort(int* tab, int n)
+{
+	int ret = 0;
+	for(int i = 1; i < n; i++)
+	{
+		ret++;
+		if(tab[i] < tab[i-1])
+		{
+			for(int j = 0; j < i; j++)
+			{
+				ret++;
+				if(tab[j] > tab[i])
+				{
+					for(;j < i; j++)
+					{
+						swap(tab + j, tab + i);
+						ret++;
+					}
+					continue;
+				}
+			}
+		}
+	}
+
+	return ret;
+}
+
+void merge(int* tab, int* tmp, int left, int mid, int right, int* cnt)
+{
+	int cntr = 0, i = left, j = mid;
+
+	while(cntr != (right - left) + 1)
+	{
+		if(i == mid)
+		{
+			tmp[cntr++] = tab[j++];
+		}
+		else if(j == right + 1)
+		{
+			tmp[cntr++] = tab[i++];
+		}
+		else if(tab[i] < tab[j])
+		{
+			tmp[cntr++] = tab[i++];
+		}
+		else if(tab[i] > tab[j])
+		{
+			tmp[cntr++] = tab[j++];
+		}
+		else
+		{
+			tmp[cntr++] = tab[i++];
+		}
+
+		(*cnt)++;
+	}
+
+	for(int k = 0; k < right - left + 1; k++)
+	{
+		tab[left + k] = tmp[k];
+		(*cnt)++;
+	}
+
+}
+
+int mergeSort(int* tab, int n)
+{
+	int* tmp = malloc(n * sizeof(int));
+	int cntr = 0;
+
+	for(int i = 1; i < n; i+=2)
+	{
+		if(tab[i] < tab[i-1])
+		{
+			swap(tab + i, tab + i - 1);
+			cntr++;
+		}
+	}
+
+	for(int i = 2; i < n; i+=2)
+	{
+		if(i == n-1)
+			merge(tab, tmp, 0, n-1, n-1, &cntr);
+		else
+			merge(tab, tmp, 0, i, i+1, &cntr);
+	}
+
+	free(tmp);
+
+	return cntr;
 }
--- TP0/fonctions.h
+++ TP0/fonctions.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void swap(int* a, int* b);
 
@@ -9,3 +11,9 @@ void affiche(int* a, int n);
 void merge (int* tab, int* tmp, int left, int mid, int right, int* cnt);
 
 int BubbleSort(int* tab, int n);
+
+int insertionSort(int* tab, int n);
+
+void merge(int* tab, int* tmp, int left, int mid, int right, int* cnt);
+
+int mergeSort(int* tab, int n);
